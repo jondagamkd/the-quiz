f4b931f781e0bd26ab4492da7d0c18ad666c4b79
jondagamkd <johntaylorwoodland@gmail.com>
2021-08-20 15:46:11 -0700

    final draft

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # the-quiz
+A fun little quiz about javascript that will test your knowledge.  This quiz is timed and you will lose time when you click a wrong answer.
+
+RACE TO THE FINISH!  The faster you get through the quiz, the higher your scores!  
+
+A link to the quiz can be found [here](https://jondagamkd.github.io/portfolio/).
+
+This site was built using [The Visual Studio Code Editor](https://code.visualstudio.com/) and [GitHub Pages](https://pages.github.com/).
+
+All projects within are the property of John T. Woodland.
+
+![the-quiz](./assets/images/quiz.jpg)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,3 @@
-var bodyEL = document.querySelector("#body");
-var highScoresEL = document.querySelector("#highScores");
-var timeLeftEL = document.querySelector("#timeLeft");
-var beginEL = document.querySelector("#begin");
-var mainDivEL = document.querySelector("#mainDiv");
-
 var countDown = 0
 var previousScores = 0
 var questionRound = 1
@@ -12,17 +6,22 @@ var seconds = 75
 var index = 0
 var stopGame = 0
 
+//Load in Questions and Answers
 var QA = [
-  {'q':'tacos?', 'a1':'tacos', 'a2':'llamas', 'a3':'burritos', 'a4':'zebras'},
-  {'q':'tacobell?', 'a1':'pandas', 'a2':'blue', 'a3':'black', 'a4':'orange'}
+  {'q':'Which is not a JavaScript Data Type?', 'a1':'Number', 'a2':'Nill', 'a3':'Boolean', 'a4':'Object'},
+  {'q':'Which symbol is used for comments in Javascript?', 'a1':'Line', 'a2':'Comment', 'a3':'double forward slash', 'a4':'double hyphen'},
+  {'q':'In Javascript what is negative Infinity?', 'a1':'A number derived by dividing a negative number by zero.', 'a2':'Any number times zero.', 'a3':'Any number times infinity and beyond.', 'a4':'A negative number derived from zero plus its square root.'},
+  {'q':'Which company developed JavaScript?', 'a1':'Google', 'a2':'IBM', 'a3':'Netscape', 'a4':'AOL'},
+  {'q':'Does JavaScript support automatic type conversion?', 'a1':'no', 'a2':'depends on the situation', 'a3':'yes', 'a4':'only when specified in code'},
+  {'q':'Which is not a looping structure in JavaScript?', 'a1':'For When', 'a2':'For', 'a3':'While', 'a4':'Do-While'},
+  {'q':'In Javascript, which function converts a string to integer?', 'a1':'String()', 'a2':'tostring()', 'a3':'str()', 'a4':'format_string'},
+  {'q':'In Javascript, What would be the result of 3+2+"7"?', 'a1':'12', 'a2':'42', 'a3':'null', 'a4':'57'}
 
 ]
-
-var answerArray = ['llamas', 'blue']
-
-timeLeftEL.textContent = "Time: 0";
+var answerArray = ['Nill', 'double forward slash', 'A number derived by dividing a negative number by zero.', 'Netscape', 'yes', 'For When', 'String()', '57' ]
 
 
+//Load previous highscores if any
 var loadScores = function() {
   scores = JSON.parse(localStorage.getItem("scores"));
 
@@ -35,10 +34,12 @@ var loadScores = function() {
 }
 loadScores()
 
+//Function to save scores to localStorage
 var saveScores = function() {
   localStorage.setItem("scores", JSON.stringify(scores));
 };
 
+//Function to store names and scores into temp memory from input and end game.
 function storeName() {
   var message = document.getElementById("message").value;
   if (!scores) {
@@ -46,13 +47,38 @@ function storeName() {
       aaa: 0
     };
   };
-  scores[message] = seconds
-  saveScores()
-
-  alert("WOOOT! You got " + seconds + "points!")
+  if (message !== "") {
+    scores[message] = seconds
+    saveScores()
+    document.getElementById("mainDiv").innerHTML = "<h1>Thank you for playing!</h1>"
+    alert("WOOOT! You got " + seconds + " points!");
+  }
+  else {
+    alert("Please enter a valid name.")
+  }
 }
 
+var bodyEL = document.querySelector("#body");
+var highScoresEL = document.querySelector("#highScores");
+var timeLeftEL = document.querySelector("#timeLeft");
+var beginEL = document.querySelector("#begin");
+var mainDivEL = document.querySelector("#mainDiv");
 
+timeLeftEL.textContent = "Time: 0";
+
+//High scores button at the top left.
+highScoresEL.addEventListener('click', function() {
+  editedStr = JSON.stringify(scores, null, 1); 
+  editedStr = editedStr.replace(/:/g, " =");
+  editedStr = editedStr.replace(/,/g, "");
+  editedStr = editedStr.replace(/"/g, "");
+  editedStr = editedStr.replace("{", "High Scores!");
+  editedStr = editedStr.replace("}", "");
+  console.log(editedStr); 
+  alert(editedStr);
+});
+
+//Begin the game upon click the main BEGIN button
 beginEL.addEventListener('click', function() {
   var setInt = setInterval(secondsFunc, 1000);
   function secondsFunc() {
@@ -63,12 +89,13 @@ beginEL.addEventListener('click', function() {
     else if (seconds <= 0 || stopGame) {
       clearInterval(setInt);
       timeLeftEL.textContent = "Time: " + seconds;
-      document.getElementById("mainDiv").innerHTML = "<h1>Times UP!</h1>";
-      document.getElementById("mainDiv").innerHTML += "<input type='text' id = 'message'><input type='submit' onclick='storeName()' value='submit' />";
+      document.getElementById("mainDiv").innerHTML = "<h1>All done!</h1><h2>Please enter your name below to save your high score.</h2>";
+      document.getElementById("mainDiv").innerHTML += "<input type='text' id = 'message'><input id = 'sbutton' type='submit' onclick='storeName()' value='submit' />";
     }
     return seconds
   } 
 
+  //Main question loop to cycle through questions object
   function questionLoop(index, finalAnswer) {  
     document.getElementById("mainDiv").innerHTML = 
     "<h2>" + QA[index]['q'] + "</h2>";
